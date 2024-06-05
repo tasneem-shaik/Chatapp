2ea5c1b109b57f533f56469f6c16cbe38878ea27
tasneem-shaik <112657672+tasneem-shaik@users.noreply.github.com>
2024-06-05 06:14:19 -0500

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+// app.get("/",(req, res)=>{
+//     res.send(" server ready!!!")
+// });
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server running in port ${PORT}`)
+});
